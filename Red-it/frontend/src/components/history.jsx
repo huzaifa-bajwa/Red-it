1e0c45b81f2c1fbb0afe9566e3a79cb173616311
Mohit Kumar <mohitkumar@MacBook-Air.local>
2024-04-19 17:39:18 +0500

    Presentation content and History Functionality added.

---
+++ Red-it/frontend/src/components/history.jsx
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import "./history.css";
+
+function History() {
+    const [fontSize, setFontSize] = useState(14);
+    const [fontSize2, setFontSize2] = useState(16); // Initial font size
+    const [content, setContent] = useState([]); // Holds the parsed content
+    const [contentType, setContentType] = useState(""); // Tracks the type of content (summary, flashcard, presentation)
+    const [summaryIndex, setSummaryIndex] = useState(0); // Tracks the index of the current content
+    const [contentArray, setContentArray] = useState([]);
+    const [typeArray, setTypeArray] = useState([]);
+
+
+    useEffect(() => {
+        const email = localStorage.getItem('userEmail');
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/history/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email }),
+                });
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.json();
+                console.log('History loaded successfully:', data.history);
+                // Parse the content based on type
+                setContentArray(data.history[0]);
+                setTypeArray(data.history[1]);
+                const [contentArraytemp, typeArraytemp] = data.history;
+
+                console.log('Content Array:', contentArray);
+                console.log('Type Array:', typeArray);
+                if (typeArraytemp[0] === "summary") {
+                    setContentType(typeArraytemp[0]);
+                    setContent(contentArraytemp[0]);
+                } else {
+                    setContent(parseData(contentArraytemp[0]));
+                    setContentType(typeArraytemp[0]);
+                }
+            } catch (error) {
+                console.error("Error fetching History:", error);
+                setContent([{ title: "Error", content: "Error loading History" }]);
+            }
+        };
+    
+        fetchData();
+    }, []);
+
+    const increaseFontSize = () => {
+        setFontSize((prevFontSize) => prevFontSize + 1);
+        setFontSize2(prevFontSize => prevFontSize + 1); // Increase font size by 1
+    };
+
+    const decreaseFontSize = () => {
+        setFontSize((prevFontSize) => Math.max(1, prevFontSize - 1));
+        setFontSize2(prevFontSize => (prevFontSize > 1 ? prevFontSize - 1 : prevFontSize)); // Decrease font size by 1, but never below 1
+    };
+
+    const cardStyle = {
+        fontSize: `${fontSize}px` // Set font size dynamically
+    };
+    const titleStyle = {
+        fontSize: `${fontSize2}px` // Set font size dynamically
+    };
+
+    const nextContent = () => {
+        const nextIndex = (summaryIndex + 1) % contentArray.length;
+        setSummaryIndex(nextIndex);
+        setContentType(typeArray[nextIndex]);
+        const nextContentData = contentArray[nextIndex];
+        if (typeArray[nextIndex] === "summary") {
+            setContent(nextContentData);
+        } else {
+            setContent(parseData(nextContentData));
+        }
+    };
+
+    const previousContent = () => {
+        const prevIndex = (summaryIndex - 1 + contentArray.length) % contentArray.length;
+        setSummaryIndex(prevIndex);
+        setContentType(typeArray[prevIndex]);
+        const prevContentData = contentArray[prevIndex];
+        if (typeArray[prevIndex] === "summary") {
+            setContent(prevContentData);
+        } else {
+            setContent(parseData(prevContentData));
+        }
+    };
+
+    const parseData = (arr) => {
+        return arr.map(str => {
+            const splitIndex = str.indexOf(":");
+            return {
+                title: str.substring(0, splitIndex),
+                content: str.substring(splitIndex + 2)
+            };
+        });
+    };
+
+    return (
+        <div className="main3">
+            <div className="bdy3">
+                <div className="content-box3">
+                    <div className="header3">
+                        <span className="title3">{contentType ? contentType.toUpperCase() : ''}</span>
+                        <span className="font-adjusters">
+                            <button className="font-decrease" onClick={decreaseFontSize}>
+                                A-
+                            </button>
+                            <button className="font-increase" onClick={increaseFontSize}>
+                                A+
+                            </button>
+                        </span>
+                    </div>
+                    <div className="summary-container3">
+                        {/* Render content based on type */}
+                        {contentType === "summary" ? (
+                            <p className="summary-text3" style={{ fontSize: `${fontSize}px` }}>
+                                {content}
+                            </p>
+                        ) : (
+                            <div className="card-grid">
+                                {content.map((item, index) => (
+                                    <div className="card" key={index}>
+                                        <span style={titleStyle} className="title">{item.title || "No Title"}</span>
+                                        <p style={cardStyle}>{item.content || "No Content"}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <div className="navigation-buttons">
+                        <button onClick={previousContent}>Previous</button>
+                        <button onClick={nextContent}>Next</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default History;
---
+++ Red-it/frontend/src/components/presentation.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react';
+import './presentation.css';
+
+function Presentation() {
+    const [fontSize, setFontSize] = useState(14); // Initial font size
+    const [fontSize2, setFontSize2] = useState(16); // Initial font size
+    const [flashcards, setFlashcards] = useState([]);
+    const [currentUrl, setCurrentUrl] = useState("");
+
+    const increaseFontSize = () => {
+        setFontSize(prevFontSize => prevFontSize + 1); // Increase font size by 1
+        setFontSize2(prevFontSize => prevFontSize + 1); // Increase font size by 1
+    };
+
+    const decreaseFontSize = () => {
+        setFontSize(prevFontSize => (prevFontSize > 1 ? prevFontSize - 1 : prevFontSize)); // Decrease font size by 1, but never below 1
+        setFontSize2(prevFontSize => (prevFontSize > 1 ? prevFontSize - 1 : prevFontSize)); // Decrease font size by 1, but never below 1
+    };
+
+    function fetchCurrentTabUrl() {
+        return new Promise((resolve, reject) => {
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                if (chrome.runtime.lastError) {
+                    reject(new Error(chrome.runtime.lastError.message));
+                } else if (tabs.length === 0) {
+                    reject(new Error("No active tab found"));
+                } else {
+                    console.log(tabs[0].url);
+                    resolve(tabs[0].url);
+                }
+            });
+        });
+    }
+
+    useEffect(() => {
+        fetchCurrentTabUrl()
+            .then((url) => {
+                const email = localStorage.getItem('userEmail');
+                setCurrentUrl(url);
+                fetch("http://127.0.0.1:8000/presentation/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        url: url,
+                        email:email,
+                    }),
+                })
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error("Network response was not ok");
+                        }
+                        return response.json();
+                    })
+                    .then((data) => {
+                        console.log(data.presentation);
+                        // setFlashcards(data.flashcards);
+                        const parsedData = parsedata(data.presentation);
+                        console.log(parsedData);
+                        setFlashcards(parsedData);
+                    })
+                    .catch((error) => {
+                        console.error("Error fetching presentation content: " + error);
+                    });
+            })
+            .catch((error) => {
+                console.error("Error fetching current tab URL: " + error);
+            });
+    }, []);
+
+    const parsedata = (arr) => {
+        return arr.map(str => {
+            const splitIndex = str.indexOf(":");
+            return {
+                title: str.substring(0, splitIndex),
+                content: str.substring(splitIndex + 2)
+            };
+        });
+    };
+
+    const cardStyle = {
+        fontSize: `${fontSize}px` // Set font size dynamically
+    };
+    const titleStyle = {
+        fontSize: `${fontSize2}px` // Set font size dynamically
+    };
+
+    return (
+        <div className="bdy5">
+            <div className="content-box5">
+                <div className="header5">
+                    <span className="title5">Presentation Content</span>
+                    <span className="font-adjusters">
+                        <button className="font-decrease" onClick={decreaseFontSize}>A-</button>
+                        <button className="font-increase" onClick={increaseFontSize}>A+</button>
+                    </span>
+                </div>
+                <div className="card-grid">
+                    {flashcards.map((flashcard, index) => (
+                        <div className="card" key={index}>
+                            <span style={titleStyle} className="title">{flashcard.title || "No Title"}</span>
+                            <p style={cardStyle}>{flashcard.content || "No Content"}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Presentation;
